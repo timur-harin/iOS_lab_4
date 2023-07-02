cd1da620dd59912d46de127db65c0e65fc88c3e2
Timur Kharin <timurx404@gmail.com>
2023-07-02 18:33:42 +0300

    Initial Commit

---
+++ Lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Timur Kharin on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
