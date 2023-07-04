2e41c00c39fbb055d67b09006d638cf7ca58de8e
Timur Kharin <timurx404@gmail.com>
2023-07-04 13:41:56 +0300

    first commit

---
+++ Lab4/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  Lab4
+//
+//  Created by Timur Kharin on 02.07.2023.
+//
+
+import Foundation
---
+++ Lab4/CharacterDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterDetailViewController.swift
+//  Lab4
+//
+//  Created by Timur Kharin on 02.07.2023.
+//
+
+import Foundation
